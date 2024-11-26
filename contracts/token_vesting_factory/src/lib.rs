8a36273653f1bb3b501a0406c233891e7c566bda
TAdev0 <angles.tristan@protonmail.com>
2024-11-26 17:21:43 +0100

    add factory contract/crate

---
+++ contracts/token_vesting_factory/src/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+use soroban_sdk::{
+    contract, contractimpl, contracttype, symbol_short, Address, Env, Map, String, Symbol, Vec,
+    U256,
+};
+
+#[contract]
+pub struct TokenVestingFactory;
+
+#[contractimpl]
+impl TokenVestingFactory {
+}
+
+mod test;
