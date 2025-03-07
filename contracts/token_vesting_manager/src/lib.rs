3ca8a060421e184f75be3c5d9e7b9187f8308dbc
TAdev0 <angles.tristan@protonmail.com>
2025-03-07 13:34:30 +0100

    fmt

--- contracts/token_vesting_manager/src/lib.rs
+++ contracts/token_vesting_manager/src/lib.rs
@@ -156,7 +156,10 @@ impl TokenVestingManager {
         // Access control check
         Self::admin_check(caller.clone(), admins.clone());
 
-        assert!(initial_unlock >= 0 && cliff_amount >= 0 && linear_vest_amount >= 0, "Invalid amount");
+        assert!(
+            initial_unlock >= 0 && cliff_amount >= 0 && linear_vest_amount >= 0,
+            "Invalid amount"
+        );
         assert!(
             linear_vest_amount + cliff_amount != 0,
             "Invalid vested amount"
@@ -853,7 +856,6 @@ impl TokenVestingManager {
         if !admins.get(caller.clone()).unwrap_or(false) {
             panic!("Not an admin");
         }
-
     }
 }
 
